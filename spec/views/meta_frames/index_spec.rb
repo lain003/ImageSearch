40326200a384496011bd6fab2105f641be234467
lain003 <a.iwakuralain@gmail.com>
2021-02-18 06:23:33 +0900

    systemテストが失敗する不具合を修正 (#33)
    systemテストが失敗する不具合を修正

--- spec/views/meta_frames/index_spec.rb
+++ spec/views/meta_frames/index_spec.rb
@@ -1,38 +1,38 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'google/cloud/storage'
 
-RSpec.feature 'MetaFramesIndex', type: :system, js: true, elasticsearch: true do
+RSpec.describe 'MetaFramesIndex', type: :system, elasticsearch: true do
   let!(:meta_frame_welcome) { create :meta_frame, text: 'ようこそ' }
   let!(:meta_frame_hello) { create :meta_frame, text: 'こんにちは' }
 
   before(:each) do
     allow_any_instance_of(MetaFrame).to receive(:image_url)
-      .and_return('http://image.imagesearch.biz/Chuunibyou/1/2/screenshot/15335.jpg')
+      .and_return('/test/sample.jpg')
     allow_any_instance_of(MetaFrame).to receive(:gif_url)
-      .and_return('http://image.imagesearch.biz/Chuunibyou/1/2/gif/15335.gif')
+      .and_return('/test/sample.gif')
+
+    visit meta_frames_index_path
   end
 
-  scenario 'デフォルトでは「ようこそ」の検索結果が表示される' do
-    visit meta_frames_index_path
+  it 'デフォルトでは「ようこそ」の検索結果が表示される' do
     expect(all('.meta-frame').count).to eq 1
     expect(all('.meta-frame').first['test_data']).to eq meta_frame_welcome.id.to_s
   end
 
-  scenario '画像をクリックするとGifが拡大表示される' do
-    visit meta_frames_index_path
+  it '画像をクリックするとGifが拡大表示される' do
     find('img.anime').click
     expect(page).to have_css('.popup_inner img')
   end
 
-  scenario '検索ボックスに単語を入力すると検索できる' do
-    visit meta_frames_index_path
+  it '検索ボックスに単語を入力すると検索できる' do
     fill_in 'words', with: 'こんにちは'
     click_button 'Search'
     expect(find('.meta-frame')['test_data']).to eq meta_frame_hello.id.to_s
   end
 
-  scenario 'Twitterアイコンを押すと、イメージとGifを共有するボタンが出る' do
-    visit meta_frames_index_path
+  it 'Twitterアイコンを押すと、イメージとGifを共有するボタンが出る' do
     image_url = 'https://twitter.com/share?text=&url=http://127.0.0.1/meta_frames/' +
                 meta_frame_welcome.id.to_s
     gif_url = 'https://twitter.com/share?text=&url=http://127.0.0.1/meta_frames/' +
@@ -47,8 +47,9 @@ RSpec.feature 'MetaFramesIndex', type: :system, js: true, elasticsearch: true do
       create_list(:meta_frame, 20, text: 'ようこそ')
     end
 
-    scenario '最後までスクロールすると、データが追加される' do
+    it '最後までスクロールすると、データが追加される' do
       visit meta_frames_index_path
+
       expect do
         page.execute_script 'window.scrollBy(0,$(document).height())'
         wait_for_block { all('.meta-frame')[20] }
